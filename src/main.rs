c4bcae772233cb9c8aee22dfbf4c4bc6ca06c092
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2026-02-16 23:07:24 +0100

    test: array slice 1

--- src/main.rs
+++ src/main.rs
@@ -1,176 +1,53 @@
-// fn main(){
+/*
+Define a `cereals` array with 5 heap Strings
+  - Cookie Crisp
+  - Cinnamon Toast Crunch
+  - Frosted Flakes
+  - Cocoa Puffs
+  - Captain Crunch
 
-//     let cake = bake_cake();
-
-//     println!("{cake}")
-
-// }
-
-// fn bake_cake()-> String {
-//     String::from ("chocolate cake")
-// }
-
-
-// fn main() {
-//     let is_correct = true;
-//     let is_event = is_correct;
-
-//     let is_booked = is_correct;
-
-//     println!("{is_correct},{is_event}, {is_booked}");
-
-//     // no
-
-//     let sushi = "Salmon";
-//     let dinner = sushi;
-
-//     let _food = sushi;
-
-//     println!("{sushi},{dinner}");
-//     // no
-
-//     let sushi = String::from("shushi");
-//     let _dinner = sushi;
-
-//     // println!("{sushi1}{dinner1}");
-//     // yes
-
-//     let food = add_to_menu(String::from("salmon"));
-
-//     println!("{food}");
-//     menu(&food);
-// }
-
-// fn main() {
-//     let mut food = String::from ("Salmon");
-//     add_to_menu(&mut food);
-
-//     println!("{food}");
-//     menu(&food);
-// }
-
-// fn add_to_menu(meal: &mut String)  {
-//     meal.push_str(" Rice");
-// }
-
-// fn menu(meal: &String) {
-//     println!("this is menu {meal}");
-// }
-
-
-// fn main(){
-//     let mut car = String::from ("BMW");
-//     let ref1 = &mut car;
-//     let ref2 = &mut car;
-
-//     println!("{ref2}");
-// }
-
-// fn main(){
-//     let _tea = make_tea();
-
-// }
-
-// fn make_tea() -> String{
-//     String::from("british")
-// }
-
-// fn main(){
-//     let level = (String::from("100"),String::from("200"));
-//     let first_level = & level.0;
-
-//     let class= vec![String::from("1"),String::from("1"),String::from("1")];
-//     let first = &class[0];
-
-//     println!("this is first {first}, this is class{class:?}");
-
-//     println!("{first_level}, {level:?}");
-
-// }
-
-// fn main(){
-
-//     let mut trip = start_trip();
-//     visit_philli(&mut trip);
-//     trip.push_str(" and ");
-//     visit_ny(&mut trip);
-//     trip.push_str(" and ");
-//     visit_bst(&mut trip);
-//     trip.push('.');
-
-//     show_art(&trip);
-
-// }
-
-// fn start_trip() ->String{
-//     String::from("this trip is")
-// }
-
-// fn visit_philli(trip:&mut String){
-//     trip.push_str(" Phili");
-// }
-
-// fn visit_ny(trip:&mut String){
-//     trip.push_str("New y");
-// }
-
-// fn visit_bst(trip:&mut String){
-//     trip.push_str("boston");
-// }
-
-// fn show_art(trip: &String){
-//     println!("{trip}")
-// }
-
-// fn main(){
-//     let name = "John Doe";
-//     let ref_name = &name;
-
-//     let first_name = &name[0..=3];
-//     let last_name = &name[5..];
-
-//     println!("{first_name} {last_name}");
-// }
-
-// fn main(){
-//       let name = "John Doe";
-//     let ref_name = &name;
-
-//     let first_name = &name[..4];
-//     let last_name = &name[5..];
-
-//     let full_name = &name[..];
-
-//     println!("{first_name} {last_name} \nthis is full name {full_name}");
-// }
-
-// fn main(){
-//     let area = String::from("Lamingo");
-//     let another_area = "Haske";
-//     go_to_area(&another_area);
-//     go_to_area(&area);
-// }
-
-// fn go_to_area(area: &str){
-//     println!("going to {area}")
-// }
-
-// fn main(){
-//     let names = ["john", "doe", "jane", "joe"];
-//     let first = &names[..=1];
-
-//     println!("this is first studen {first:?}")
-// }
+ 
+Declare a `first_two` variable that extracts a slice
+of the first two cereals. Print the slice.
+ 
+Declare a `mid_three` variable that extracts a slice
+of the middle three cereals (Cinnamon Toast Crunch
+up to and including Cocoa Puffs). Print the slice.
+ 
+Declare a `last_three` variable that extracts a slice
+of the last three cereals. Print the slice.
+ 
+Using the `last_three` slice, target the last element
+("Captain Crunch") and replace it with "Lucky Charms".
+Print the complete `cereals` array.
+ 
+Declare a `cookie_crisp` variable that references the
+"Cookie Crisp" String.
+ 
+Declare a `cookie` variable that extracts a slice of
+the text "Cookie" from the String. Print the slice.
+ 
+Declare a `cocoa_puffs` variable. Make it a reference
+to the "Cocoa Puffs" String (in other words, a &String).
+ 
+Declare a `puffs` variable that extracts a slice of
+the text "Puffs" from the String. Print the slice.
+*/
 
 fn main(){
-    let mut array:[i32; 5] = [1,2,3,4,5];
-    let slice = &mut array[1..4];
+    let mut cereals:[String;5] = [String::from("cookie Crips"),String::from("Cinnamon Toast Crunch"),String::from("Frosted Flakes"),String::from("Cocoa Puffs"),String::from("Captain Crunch")];
 
-    println!("{slice:?}");
+    let first_two = &cereals[0..=1];
+    println!("{first_two:?}");
 
-    slice[0]=100;
+    let mid_three = &cereals[1..=3];
+    println!("{mid_three:?}");
 
-    println!("{slice:?}");
-    println!("{array:?}");
+    let last_three = &mut cereals[2..];
+    println!("{last_three:?}");
 
+    last_three[2] = String::from ("Lucky Charms");
+
+    println!("this is cereals {cereals:?}");
 }
+

489d7a5a6fa811ea8e73c7404a48958eadb5c04c
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2026-02-17 00:38:30 +0100

    struct functions

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
-fn main(){
     struct Staff {
         name: String,
         age: u32,
         is_admin: bool,
     }
-
-    let mut john = Staff{
-        age: 32,
-        name: String::from("John Doe"),
-        is_admin:false
-    };
-
-    let johns_age = john.age;
-
-    john.name = String::from("John David");
-    john.age =33;
-    john.is_admin = true;
+fn main(){
 
 
+    // let mut john = Staff{
+    //     age: 32,
+    //     name: String::from("John Doe"),
+    //     is_admin:false
+    // };
 
-    println!("{} is {} and his admin status is {}", john.name, john.age, john.is_admin);
+    // let johns_age = john.age;
+
+    // john.name = String::from("John David");
+    // john.age =33;
+    // john.is_admin = true;
 
 
+
+    // println!("{} is {} and his admin status is {}", john.name, john.age, john.is_admin);
+
+
+    let staff_name =String::from("jane doe");
+
+    let new_staff = add_staff(staff_name, 32,true);
+
+    println!("the new staff {} is {} and admin stats is {}",new_staff.name, new_staff.age, new_staff.is_admin);
+
+
+}
+
+fn add_staff(name:String, age:u32, is_admin:bool)-> Staff{
+    Staff { 
+        name:name, 
+        age:age, 
+        is_admin:is_admin 
+    }
 }

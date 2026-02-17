085dc2066e504a66fffa26a4dbf12415fa51584f
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2026-02-18 00:30:37 +0100

    tuple struct

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,50 @@
-#[derive(Debug)]
+
+
+// impl Pc {
+//     fn new(name:String, year:u32, memory:u32, drive:u32, core:String)->Self{
+//         Self { name, year, memory, drive, core }
+//     }
+// }
+
+// impl Pc {
+//     fn upgrade_memory(&mut self, new_mem: u32)->&mut Self{
+//         self.memory = new_mem;
+//         self
+//     }
+
+//     fn upgrade_drive(&mut self, new_drive: u32) -> &mut Self{
+//         self.drive = new_drive;
+//         self
+//     }
+
+//     fn upgrade_core(&mut self, new_core:String)-> &mut Self{
+//         self.core=new_core;
+//         self
+//     }
+// }
+// fn main(){
+//     let mut mac= Pc::new(String::from("mac"), 2018, 4, 256, String::from("i5"));
+
+
+
+//     println!("specs: {mac:#?}");
+
+//     mac.upgrade_core(String::from("i7"))
+//     .upgrade_drive(500)
+//     .upgrade_memory(16);
+
+//     println!("new specs: {mac:#?}");
+// }
+
+
 #[allow(unused)]
 
-struct Pc{
-    name: String,
-    year: u32,
-    core: String,
-    memory: u32,
-    drive: u32,
-}
-
-impl Pc {
-    fn new(name:String, year:u32, memory:u32, drive:u32, core:String)->Self{
-        Self { name, year, memory, drive, core }
-    }
-}
-
-impl Pc {
-    fn upgrade_memory(&mut self, new_mem: u32)->&mut Self{
-        self.memory = new_mem;
-        self
-    }
-
-    fn upgrade_drive(&mut self, new_drive: u32) -> &mut Self{
-        self.drive = new_drive;
-        self
-    }
-
-    fn upgrade_core(&mut self, new_core:String)-> &mut Self{
-        self.core=new_core;
-        self
-    }
-}
+struct Time(u32,u32);
+struct Year(u8, String, u32);
 fn main(){
-    let mut mac= Pc::new(String::from("mac"), 2018, 4, 256, String::from("i5"));
+    let check_in=Time(10,30);
+    println!("check_in is at {} : {} am", check_in.0, check_in.1);
 
-
-
-    println!("specs: {mac:#?}");
-
-    mac.upgrade_core(String::from("i7"))
-    .upgrade_drive(500)
-    .upgrade_memory(16);
-
-    println!("new specs: {mac:#?}");
+    let event_date = Year(2, String::from("January"), 2026);
+    println!("the event is slated for {},{},{}", event_date.0, event_date.1, event_date.2);
 }

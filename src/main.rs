74defa8f4e0ca75615048255991780a1295a8ca8
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2026-02-19 21:58:58 +0100

    varient struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-
 #[derive(Debug)]
-#[allow(unused,unused_variables)]
+#[allow(unused, unused_variables)]
 
-struct Credentials{
+struct Credentials {
     username: String,
     password: String,
 }
@@ -11,20 +10,21 @@ struct Credentials{
 enum Payment {
     CreditCard(String),
     DebitCard(Credentials),
-    Wallet(String),
+    Wallet { address: String, user_id: u32 },
 }
-fn main(){
-
-    let credents = Credentials{
-        username: String::from ("doe@mail.com"),
-        password: String::from ("12345")
+fn main() {
+    let credents = Credentials {
+        username: String::from("doe@mail.com"),
+        password: String::from("12345"),
     };
 
     let mut pay_with_card = Payment::CreditCard(String::from("540-60-12"));
     let pay_debit = Payment::DebitCard(credents);
 
-     println!("{:?}\n{:?}", pay_with_card, pay_debit);
+    let use_wallet = Payment::Wallet {
+        address: String::from("0x1111"),
+        user_id: (1),
+    };
 
-
-
+    println!("{:?}\n{:?}\n{:#?}", pay_with_card, pay_debit, use_wallet);
 }

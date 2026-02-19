bd14bb6af012f7408014a4ddcaf290267e062b8f
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2026-02-19 21:33:27 +0100

    multi associated data

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 
 #[derive(Debug)]
 #[allow(unused,unused_variables)]
-enum CardSuit {
-    Hearts(String),
-    Diamonds(String),
-    Spades(String),
-    Clubs(String),
-}
-
-struct Card {
-    rank: String,
-    suit: CardSuit,
+enum Payment {
+    CreditCard(String),
+    DebitCard(String, String),
+    Wallet(String),
 }
 fn main(){
-    let firstCard= CardSuit::Hearts(String::from ("10"));
+    let mut pay_with_card = Payment::CreditCard(String::from("540-60-12"));
+     pay_with_card = Payment::DebitCard(String::from("James"), String::from("231-099-890"));
+
+     println!("{:?}", pay_with_card)
 
 
-    println!("{:?}", firstCard);
-    
+
 }

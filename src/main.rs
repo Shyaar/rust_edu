5dbe0608799083a40b9187836e5d6165b3553c34
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2026-02-19 23:09:58 +0100

    enum nestin

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,60 @@
 #[derive(Debug)]
 #[allow(unused, unused_variables)]
 
-struct Credentials {
-    username: String,
-    password: String,
+// struct Credentials {
+//     username: String,
+//     password: String,
+// }
+// #[derive(Debug)]
+
+// enum Payment {
+//     CreditCard(String),
+//     DebitCard(Credentials),
+//     Wallet { address: String, user_id: u32 },
+// }
+
+// impl Payment{
+
+//     fn new(address:String, user_id:u32)->Self{
+//         Self::Wallet { address, user_id }
+//     }
+// }
+// fn main() {
+//     let credents = Credentials {
+//         username: String::from("doe@mail.com"),
+//         password: String::from("12345"),
+//     };
+
+//     let mut pay_with_card = Payment::CreditCard(String::from("540-60-12"));
+//     let pay_debit = Payment::DebitCard(credents);
+
+//     let use_wallet = Payment::Wallet {
+//         address: String::from("0x1111"),
+//         user_id: (1),
+//     };
+
+//     let user2 = Payment::Wallet.new();
+
+//     println!("{:?}\n{:?}\n{:#?}", pay_with_card, pay_debit, use_wallet);
+// }
+
+
+enum Meat {
+    Chiken,
+    Beef,
 }
+
 #[derive(Debug)]
-
-enum Payment {
-    CreditCard(String),
-    DebitCard(Credentials),
-    Wallet { address: String, user_id: u32 },
+enum Meal {
+    Sharawma(Meat),
+    SandWich(Meat),
+    Stew,
 }
-fn main() {
-    let credents = Credentials {
-        username: String::from("doe@mail.com"),
-        password: String::from("12345"),
-    };
 
-    let mut pay_with_card = Payment::CreditCard(String::from("540-60-12"));
-    let pay_debit = Payment::DebitCard(credents);
+fn main(){
+    let order = Meal::SandWich(Meat::Beef);
+    let order2= Meal::Sharawma(Meat::Chiken);
 
-    let use_wallet = Payment::Wallet {
-        address: String::from("0x1111"),
-        user_id: (1),
-    };
-
-    println!("{:?}\n{:?}\n{:#?}", pay_with_card, pay_debit, use_wallet);
+    println!("order 1 :::: {:?}", order);
+    println!("Order 2 >>>> {:?}", order2);
 }
